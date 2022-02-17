8e43787f315f9be4c4a07af7a91467a6b40e6bdb
Amkam <amaduskamara@gmail.com>
2022-02-17 08:20:47 +0000

    Add 5 owners into owners table

--- data.sql
+++ data.sql
@@ -14,3 +14,8 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 
 /* Insert the data into the owners table: */
 INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
