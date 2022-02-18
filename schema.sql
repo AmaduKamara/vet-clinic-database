138f0a7a0429c59a064ddb19b10a39213fdc6a69
Amkam <amaduskamara@gmail.com>
2022-02-18 07:53:48 +0000

    Create a new table - vets

--- schema.sql
+++ schema.sql
@@ -39,3 +39,11 @@ ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 
 /* Add column owner_id which is a foreign key referencing the owners table */
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+
+/* CREATING A NEW TABLE - vets */
+CREATE TABLE vets(
+   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+   name VARCHAR (250) NOT NULL,
+   age  INT NOT NULL,
+   date_of_graduation DATE NOT NULL,
+);
